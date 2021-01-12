10af5d8b6ec7d757074a03cc73ae12ed9649b3e5
AQuoc98 <badguykendf@gmail.com>
2021-01-12 22:21:06 +0700

    Render element

--- lesson2-learn-reactjs/src/App.js
+++ lesson2-learn-reactjs/src/App.js
@@ -1,22 +1,57 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Ken';
+  const age = 22;
+  const isMale = true;
+  const student = {
+    name: 'Ken'
+  }
+  const colorList = ['red', 'green', 'blue']
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>Xin chao {name} - {age} - {isMale ? 'Male' : 'Female'}</p>
+
+        {isMale ? <p>Male</p> : <p>Female</p>}
+
+        {isMale && <p>Male</p>}
+        {!isMale && <p>Female</p>}
+
+        {isMale && (
+          <div>
+            <p>Male 1</p>
+            <p>Male 2</p>
+          </div>
+        )}
+
+        {isMale && (
+          <React.Fragment>
+            <p>Male 3</p>
+            <p>Male 4</p>
+          </React.Fragment>
+        )}
+
+        {isMale && (
+          <>
+            <p>Male 5</p>
+            <p>Male 6</p>
+          </>
+        )}
+
+        <p>{student.name}</p>
+
+        <ul>
+          {colorList.map((color, index) => (
+            <li key={index} style={{color}}>{color}</li>
+          ))}
+        </ul>
+
       </header>
     </div>
   );
